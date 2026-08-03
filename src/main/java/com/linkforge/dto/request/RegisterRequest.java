c9ad1c586a2e8c586fd399ae78d91841d695fdc7
Aryan Raj <aryanra2607j@gmail.com>
2026-08-03 16:59:41 +0000

    feat(auth): add authentication DTOs and password encoder

---
+++ src/main/java/com/linkforge/dto/request/RegisterRequest.java
@@ -0,0 +1,12 @@
+package com.linkforge.dto.request;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    private String fullName;
+    private String username;
+    private String email;
+    private String password;
+}
