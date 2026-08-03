2dcacdcb3c1b886c5f993b692974d54f439ffe31
Aryan Raj <aryanra2607j@gmail.com>
2026-08-03 16:57:51 +0000

    feat(user): add User entity and repository

---
+++ src/main/java/com/linkforge/entity/User.java
@@ -0,0 +1,34 @@
+package com.linkforge.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private boolean enabled = true;
+}
