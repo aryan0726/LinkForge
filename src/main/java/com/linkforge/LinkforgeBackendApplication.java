1edcaa81c2fdfdceb84cde03b0ab5e262aa25f5f
Aryan Raj <aryanra2607j@gmail.com>
2026-08-03 16:51:22 +0000

    chore: initialize Spring Boot backend project

---
+++ src/main/java/com/linkforge/LinkforgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.linkforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkforgeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkforgeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linkforge/LinkforgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkforgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
